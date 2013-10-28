97ca5e4235cb9007c23c84c9f6b396a35723510b
imshashank <shashank@shankee.com>
2013-10-28 19:07:45 -0400

    new

--- wp-content/themes/jukt-micronics-buddypress-buddypack/functions.php
+++ wp-content/themes/jukt-micronics-buddypress-buddypack/functions.php
@@ -1,3 +1,14 @@
 <?php
 define( 'BP_DTHEME_DISABLE_CUSTOM_HEADER', true );
+
+add_filter( 'wp_nav_menu_items', 'my_nav_menu_profile_link' );
+function my_nav_menu_profile_link($menu) {      
+        if (!is_user_logged_in())
+                return $menu;
+        else
+                $profilelink = '<li><a href="' . bp_loggedin_user_domain( '/' ) . '">' . __('Visit your Awesome Profile') . '</a></li>';
+                $menu = $menu . $profilelink;
+                return $menu;
+}
+
 ?>
--- wp-content/themes/jukt-micronics-buddypress-buddypack/page-define.php
+++ wp-content/themes/jukt-micronics-buddypress-buddypack/page-define.php
@@ -32,7 +32,7 @@ if(!$author_posts->have_posts()){
 $yourcat= get_category($check);
 $name=$yourcat->name;
 $new_post = array(
-'post_title' => $name,
+'post_title' => $name." Definition",
 'post_content' => 'Please enter a definition for '.$name ,
 'post_status' => 'publish',
 'post_date' => date('Y-m-d H:i:s'),
@@ -42,9 +42,54 @@ $new_post = array(
 );    
 
 
-
 $post_id = wp_insert_post($new_post); 
-echo "new definition created for $name</br>";
+ 
+if (!$key_1_values)
+{
+add_post_meta( $post_id, 'level', 1 ); 
+add_post_meta( $post_id, 'type', "student" ); 
+}
+else
+{
+$key_1_values++;
+add_post_meta( $post_id, 'level', $key_1_values ); 
+}
+
+
+echo "new definition created for $name</br> and has level". $key_1_values;
+
+//new master->
+
+
+$yourcat= get_category($check);
+$name=$yourcat->name;
+$new_post = array(
+'post_title' => $name ." Story",
+'post_content' => 'Please enter a definition for '.$name ,
+'post_status' => 'publish',
+'post_date' => date('Y-m-d H:i:s'),
+'post_author' => $user_ID,
+'post_parent' => $post_id,
+'post_type' => 'post',
+'post_category' => $array
+);    
+
+$master_id = wp_insert_post($new_post); 
+
+if (!$key_1_values)
+{
+add_post_meta( $master_id, 'level', 1 ); 
+add_post_meta( $master_id, 'type', "master" ); 
+}
+else
+{
+$key_1_values++;
+add_post_meta( $master_id, 'level', $key_1_values ); 
+}
+
+
+
+echo "new story created for $name</br> and has level". $key_1_values;
 
 
 }else echo "definition already exists for $name</br>";
@@ -60,24 +105,57 @@ echo "</div>";
 
     global $current_user;
     get_currentuserinfo();
-    $author_query = array('posts_per_page' => '-1','author' => $current_user->ID);
+    $author_query = array('posts_per_page' => '-1','orderby'=> 'title','order' => 'ASC','author' => $current_user->ID);
     $author_posts = new WP_Query($author_query);
-    while($author_posts->have_posts()) : $author_posts->the_post();echo '<h2>';?>
+	
+    while($author_posts->have_posts()) : $author_posts->the_post();
+	
+		
+	$key_1_value = get_post_meta( get_the_ID(), 'type', true );
+	// check if the custom field has a value
+	if ($key_1_value=="student")
+	{echo "<div class='prop-box'>";}
+	
+	if( ! empty( $key_1_value ) ) {
+	echo "<div class='".$key_1_value."'><h3>Type: ".$key_1_value."</h3></div>";
+	} 
+	 
+	echo '<h2>';?>
 	<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>       
+	
+	
 	<?php
+	$rating = wp_gdsr_show_article_rating	 (	 
+$post_id = 0,
+$use_default = true,
+$size = 20,
+$style = "oxygen",
+$echo = true	  
+);
+echo $rating;	
 	echo '</h2>';
 	the_content();
-    ?>
+	
+	if ($key_1_value=="master")
+	{echo "</div>";}
+	
+	
+    ?> 
         
     <?php           
     endwhile;
-
+	
 else :
 
-    echo "not logged in";
+    echo "not logged in"; 
 
 endif;
+
 	?>
+	
+	
+
+
 	
 
 		</div><!-- .page -->
